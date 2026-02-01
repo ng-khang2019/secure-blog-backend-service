4d8c2c952ade52dd5520ed8294cf36c504bed2ed
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 17:18:54 +0700

    Create rest authentication entry point for login request

---
+++ src/main/java/com/project/prjauth/config/RestAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.project.prjauth.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.prjauth.dto.response.ApiErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String message = "Unauthorized: Authentication failed!";
+
+        if (authException instanceof BadCredentialsException) {
+            message = "Invalid username or password";
+        }
+
+        ApiErrorResponse errorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message(message)
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        response.flushBuffer();
+    }
+}
