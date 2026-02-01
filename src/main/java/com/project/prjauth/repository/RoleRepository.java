d6e8aee47399d4d97f927ef2daeebeadb8f23732
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:23:42 +0700

    Create role repo

---
+++ src/main/java/com/project/prjauth/repository/RoleRepository.java
@@ -0,0 +1,21 @@
+package com.project.prjauth.repository;
+
+import com.project.prjauth.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    @Query("""
+    Select r From Role r Left Join Fetch r.permissions
+    """)
+    List<Role> findAllWithPermissions();
+
+    void deleteRoleByName(String name);
+
+    Optional<Role> findByName(String name);
+}
