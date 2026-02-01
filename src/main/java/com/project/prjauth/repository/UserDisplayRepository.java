7eb7e6ba0418fd1af52e9945d3a9545c2b19b5fd
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 15:50:52 +0700

    Add find user profile by email

--- src/main/java/com/project/prjauth/repository/UserDisplayRepository.java
+++ src/main/java/com/project/prjauth/repository/UserDisplayRepository.java
@@ -4,6 +4,7 @@ import com.project.prjauth.dto.response.UserProfileResponse;
 import com.project.prjauth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDisplayRepository extends JpaRepository<User,Long> {
     @Query("""
@@ -14,4 +15,13 @@ public interface UserDisplayRepository extends JpaRepository<User,Long> {
             Where u.id = :id
     """)
     UserProfileResponse findUserProfileById(Long id);
+
+    @Query("""
+            Select new com.project.prjauth.dto.response.UserProfileResponse(
+                u.id, u.firstName, u.lastName, u.username, i.url)
+            From User u
+            Left Join u.avatarImage i
+            Where u.email = :email
+    """)
+    UserProfileResponse findUserProfileByEmail(@Param("email") String email);
 }
