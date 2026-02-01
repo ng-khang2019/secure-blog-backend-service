cb9363ba252c4095a7d2011e4a5040121f0c9a6a
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:33:28 +0700

    Create blueprint for image controller

---
+++ src/main/java/com/project/prjauth/controller/ImageController.java
@@ -0,0 +1,12 @@
+package com.project.prjauth.controller;
+
+public class ImageController {
+
+    // Upload user image
+
+    // Delete user image
+
+    // Upload post-thumbnail
+
+    // Delete post-thumbnail
+}
