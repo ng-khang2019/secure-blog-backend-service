8d3ec82411d173c6d0d6f75772a13383efd6de38
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:25:41 +0700

    Add User display repo to call queries dto projection for displaying only necessary user details

---
+++ src/main/java/com/project/prjauth/repository/UserDisplayRepository.java
@@ -0,0 +1,17 @@
+package com.project.prjauth.repository;
+
+import com.project.prjauth.dto.response.UserProfileResponse;
+import com.project.prjauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserDisplayRepository extends JpaRepository<User,Long> {
+    @Query("""
+            Select new com.project.prjauth.dto.response.UserProfileResponse(
+                u.id, u.firstName, u.lastName, u.username, i.url)
+            From User u
+            Left Join u.avatarImage i
+            Where u.id = :id
+    """)
+    UserProfileResponse findUserProfileById(Long id);
+}
