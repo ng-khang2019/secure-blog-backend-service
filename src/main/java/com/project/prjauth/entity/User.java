ebe13c730849221dcb2d4e632eb891a02a8bfead
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:17:08 +0700

    Add User entity

---
+++ src/main/java/com/project/prjauth/entity/User.java
@@ -0,0 +1,45 @@
+package com.project.prjauth.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
+@Entity @Table(name = "users")
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User extends BaseEntity<Long> {
+
+    @Column(name = "firstName", nullable = false)
+    String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    String lastName;
+
+    @Column(name = "username", unique = true, nullable = false)
+    String username;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    String email;
+
+    @Column(name = "phone")
+    String phone;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "avatar_image_id")
+    Image avatarImage;
+
+}
