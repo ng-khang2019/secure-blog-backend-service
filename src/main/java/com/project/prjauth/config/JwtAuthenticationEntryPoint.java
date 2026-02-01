ba34c02546be188f285ab5792ce7ba515e9a8b1f
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:32:48 +0700

    Add custom jwt authentication entry point

---
+++ src/main/java/com/project/prjauth/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.project.prjauth.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.prjauth.dto.response.ApiErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiErrorResponse errorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Unauthorized: Authentication required or token is invalid")
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        response.flushBuffer();
+    }
+
+}
