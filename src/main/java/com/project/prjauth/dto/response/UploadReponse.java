21343ad24ead584de52e0006d0cbd3f632188fb0
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:21:19 +0700

    Create UploadResponse dto

---
+++ src/main/java/com/project/prjauth/dto/response/UploadReponse.java
@@ -0,0 +1,11 @@
+package com.project.prjauth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UploadReponse {
+    private String url;
+    private String publicId;
+}
