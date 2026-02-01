446f95d8eef2fffbdf917d3276900f0af24adc00
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:15:07 +0700

    Initial Commit

---
+++ src/main/java/com/project/prjauth/ProjectAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.project.prjauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/prjauth/ProjectAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prjauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
