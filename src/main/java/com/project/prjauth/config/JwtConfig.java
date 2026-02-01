540b40cdeed965dece0cb8946f2b78465c6abe11
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 14:31:26 +0700

    Add jwt decoder and its jwt authentication converter

---
+++ src/main/java/com/project/prjauth/config/JwtConfig.java
@@ -0,0 +1,51 @@
+package com.project.prjauth.config;
+
+import com.project.prjauth.service.RedisTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtConfig {
+    private final RedisTokenService redisTokenService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "HmacSHA512");
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) NimbusJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+        return token -> {
+            Jwt jwt = jwtDecoder.decode(token);
+            redisTokenService.validateNotBlackListed(jwt.getId());
+            return jwt;
+        };
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter scopeConverter = new JwtGrantedAuthoritiesConverter();
+        scopeConverter.setAuthorityPrefix("");
+        scopeConverter.setAuthoritiesClaimName("scope");
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(scopeConverter);
+
+        return converter;
+    }
+}
