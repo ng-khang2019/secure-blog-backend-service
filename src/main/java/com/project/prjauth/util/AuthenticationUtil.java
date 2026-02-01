d5f95a9541b2a9504dc57fc5d2b4ab4e1e83153b
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-01 15:46:02 +0700

    Add email extraction method

--- src/main/java/com/project/prjauth/util/AuthenticationUtil.java
+++ src/main/java/com/project/prjauth/util/AuthenticationUtil.java
@@ -3,6 +3,7 @@ package com.project.prjauth.util;
 import com.project.prjauth.entity.CustomUserDetails;
 import com.project.prjauth.entity.User;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -17,10 +18,23 @@ public class AuthenticationUtil {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
+    public static String getCurrentUserEmail() {
+        Authentication authentication = getAuthentication();
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof Jwt jwt) {
+            return jwt.getSubject();
+        }
+        if (principal instanceof UserDetails ud) {
+            return ud.getUsername();
+        }
+        throw new IllegalStateException("Cannot extract principal");
+    }
+
     public static Long getUserId() {
         Authentication authentication = getAuthentication();
         Object principal = authentication.getPrincipal();
-        if (principal instanceof CustomUserDetails) {
+        if (principal instanceof UserDetails) {
             return ((CustomUserDetails) principal).getId();
         }
         throw new IllegalStateException("Cannot extract user id from authentication object");
