eeb2b602fb15cf9592016993b60a3da19dfe7e64
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-03 12:25:00 +0700

    Create category entity

---
+++ src/main/java/com/project/prjauth/entity/Category.java
@@ -0,0 +1,24 @@
+package com.project.prjauth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Table(name = "categories")
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category extends BaseEntity<Long>{
+
+    @Column(nullable = false, unique = true)
+    String name;
+
+    @OneToMany(mappedBy = "category")
+    List<Post> posts = new ArrayList<>();
+}
