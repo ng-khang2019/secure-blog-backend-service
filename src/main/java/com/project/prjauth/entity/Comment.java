fb9ef26f0b03e19f389329ebf6d19f70331e7588
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-03 12:25:23 +0700

    Create comment entity

---
+++ src/main/java/com/project/prjauth/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.project.prjauth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Table(name = "comments")
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Comment extends BaseEntity<Long>{
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
+}
