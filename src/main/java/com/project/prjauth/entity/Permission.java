9896916fe34958e1c719f77bea8bf92647aaa642
mkn <192448446+mika0798@users.noreply.github.com>
2026-02-03 18:26:42 +0700

    remove unused packages

--- src/main/java/com/project/prjauth/entity/Permission.java
+++ src/main/java/com/project/prjauth/entity/Permission.java
@@ -1,16 +1,12 @@
 package com.project.prjauth.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-import java.util.Set;
 
 @Entity @Table(name = "permissions")
 @Getter @Setter
