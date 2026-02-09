a7314a494f520119de6471793ccc57786490fa7b
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-10 00:06:24 +0700

    Complete Comment Repository

---
+++ src/main/java/com/project/prjauth/repository/CommentRepository.java
@@ -0,0 +1,26 @@
+package com.project.prjauth.repository;
+
+import com.project.prjauth.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("""
+        Select c From Comment c
+        Where c.post.id = :postId
+        And c.parent Is Null
+        Order By c.createdAt Asc
+    """)
+    List<Comment> findRootCommentsByPostId(@Param("postId")Long postId);
+
+    @Query("""
+        Select c From Comment c
+        Where c.parent.id = :parentId
+        Order By c.createdAt Asc
+    """)
+    List<Comment> findChildrenCommentsByParentId(@Param("parentId") Long parentId);
+
+}
