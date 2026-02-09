9c8c3a51d99eb29a166fe22f731cb4e083693d94
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-09 18:51:04 +0700

    -Separate comment from base entity -Add entity listeners -Add json ignore

--- src/main/java/com/project/prjauth/entity/Comment.java
+++ src/main/java/com/project/prjauth/entity/Comment.java
@@ -1,17 +1,28 @@
 package com.project.prjauth.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity @Table(name = "comments")
 @Getter @Setter @Builder
+@EntityListeners( value = { AuditingEntityListener.class })
 @AllArgsConstructor @NoArgsConstructor
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Comment extends BaseEntity<Long>{
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     String content;
@@ -27,4 +38,12 @@ public class Comment extends BaseEntity<Long>{
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     List<Comment> children = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    LocalDateTime updatedAt;
 }
