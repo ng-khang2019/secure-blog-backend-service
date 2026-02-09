cfaa4cc718f073d08c943afe1ba89ee2d728d369
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-09 19:15:04 +0700

    Remove List and Array packages

--- src/main/java/com/project/prjauth/entity/Post.java
+++ src/main/java/com/project/prjauth/entity/Post.java
@@ -5,9 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity @Table(name = "posts")
