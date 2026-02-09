084e45bdfc9304f69b26503ecb6014106ba91248
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-10 01:02:28 +0700

    Add constraints for comment request

--- src/main/java/com/project/prjauth/dto/request/CommentRequest.java
+++ src/main/java/com/project/prjauth/dto/request/CommentRequest.java
@@ -1,6 +1,8 @@
 package com.project.prjauth.dto.request;
 
+import com.project.prjauth.annotation.WordRange;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,7 @@ import lombok.Setter;
 public class CommentRequest {
 
     @NotBlank( message = "Comment cannot be blank")
+    @Size(min = 2, message = "Comment must be at least 2 characters long")
+    @WordRange(min = 1, max = 400, message = "Comment must be between 1 and 400 words")
     String content;
 }
