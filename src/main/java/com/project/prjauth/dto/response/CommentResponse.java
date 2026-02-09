72c9fc9f5f7c75b5649eb3e88a32bd29f4efe5aa
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-09 23:03:18 +0700

    Add timestamps for comment dto

--- src/main/java/com/project/prjauth/dto/response/CommentResponse.java
+++ src/main/java/com/project/prjauth/dto/response/CommentResponse.java
@@ -3,6 +3,7 @@ package com.project.prjauth.dto.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +12,11 @@ public class CommentResponse {
 
     Long id;
     String content;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
     Long userId;
     String firstName;
     String lastName;
+
     List<CommentResponse> replies = new ArrayList<>();
 }
