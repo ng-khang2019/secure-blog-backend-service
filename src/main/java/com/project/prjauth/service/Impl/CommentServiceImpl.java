f9ac417d466fef47bfc03db125fc245d9034e700
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-10 14:18:11 +0700

    Create comment service implementation and methods blueprint

---
+++ src/main/java/com/project/prjauth/service/Impl/CommentServiceImpl.java
@@ -0,0 +1,46 @@
+package com.project.prjauth.service.Impl;
+
+import com.project.prjauth.dto.request.CommentRequest;
+import com.project.prjauth.dto.response.CommentResponse;
+import com.project.prjauth.entity.User;
+import com.project.prjauth.repository.CommentRepository;
+import com.project.prjauth.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
+
+    @Override
+    public CommentResponse createComment(Long postId, CommentRequest request, User currentUser) {
+        return null;
+    }
+
+    @Override
+    public CommentResponse replyToComment(Long postId, Long parentId, CommentRequest request, User currentUser) {
+        return null;
+    }
+
+    @Override
+    public CommentResponse editComment(Long commentId, CommentRequest request, User currentUser) {
+        return null;
+    }
+
+    @Override
+    public Page<CommentResponse> getRootComments(Long postId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Page<CommentResponse> getChildrenComments(Long parentId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public void deleteComment(Long commentId, User currentUser) {
+
+    }
+}
