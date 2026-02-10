5ddbb52950a0f3294fb956ac3cb56adce77a3f26
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-10 13:47:55 +0700

    Add create, update, reply and delete service for Comment

--- src/main/java/com/project/prjauth/service/CommentService.java
+++ src/main/java/com/project/prjauth/service/CommentService.java
@@ -1,10 +1,16 @@
 package com.project.prjauth.service;
 
+import com.project.prjauth.dto.request.CommentRequest;
 import com.project.prjauth.dto.response.CommentResponse;
+import com.project.prjauth.entity.User;
 import org.springframework.data.domain.Page;
 
 public interface CommentService {
 
+    CommentResponse createComment(Long postId, CommentRequest request, User currentUser);
+    CommentResponse replyToComment(Long postId, Long parentId, CommentRequest request, User currentUser);
+    CommentResponse editComment(Long commentId, CommentRequest request, User currentUser);
     Page<CommentResponse> getRootComments(Long postId, int page, int size);
     Page<CommentResponse> getChildrenComments(Long parentId, int page, int size);
+    void deleteComment(Long commentId, User currentUser);
 }
