fe28daa82b27bf6b8669633d295d8bf1bf76cc38
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-27 20:56:14 +0700

    Fix Bean declaring conflict!

--- src/main/java/com/project/prjauth/util/SpringSecurityAuditorAware.java
+++ src/main/java/com/project/prjauth/util/SpringSecurityAuditorAware.java
@@ -3,11 +3,9 @@ package com.project.prjauth.util;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component("springSecurityAuditorAware")
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
 
     @Override
