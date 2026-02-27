05c722bc9591fae9835ef365c8a52df67b189b08
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-02-27 22:59:07 +0700

    Complete create Comment method

--- src/main/java/com/project/prjauth/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/project/prjauth/service/Impl/CommentServiceImpl.java
@@ -2,9 +2,15 @@ package com.project.prjauth.service.Impl;
 
 import com.project.prjauth.dto.request.CommentRequest;
 import com.project.prjauth.dto.response.CommentResponse;
+import com.project.prjauth.entity.Comment;
+import com.project.prjauth.entity.Post;
 import com.project.prjauth.entity.User;
+import com.project.prjauth.exception.ResourceNotFoundException;
+import com.project.prjauth.mapper.CommentMapper;
 import com.project.prjauth.repository.CommentRepository;
+import com.project.prjauth.repository.PostRepository;
 import com.project.prjauth.service.CommentService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -13,10 +19,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final CommentMapper CommentMapper;
 
     @Override
+    @Transactional
     public CommentResponse createComment(Long postId, CommentRequest request, User currentUser) {
-        return null;
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post not found"));
+        Comment comment = Comment.builder()
+                .content(request.getContent())
+                .user(currentUser)
+                .post(post)
+                .parent(null)
+                .build();
+
+        return CommentMapper.toCommentResponse(commentRepository.save(comment));
     }
 
     @Override
